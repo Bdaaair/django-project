849d066212ed42d281fef96082d53d8abe4be414
Bader Al-Faraj <Bdaaair>
2023-01-28 20:35:17 +0300

    not yet

--- coded/models.py
+++ coded/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 
 # Create your models here.
-class Coded(models.Model):
+
+
+class Event(models.Model):
+    name = models.CharField(max_length=150)
+    image = models.ImageField()
+    organiser = models.CharField(max_length=150)
     Date = models.DateField()
     title = models.CharField(max_length=100)
     body = models.TextField()
